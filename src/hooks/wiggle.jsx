2b8d06ca5d88e1618930bce678c180ae35cc7ed3
Glitch (glitch-hello-react) <none>
2024-11-10 17:19:28 +0000

    🏭👮 Checkpoint ./package.json:71694311/3449 ./src/pages/home.jsx:71694311/200 ./src/hooks/wiggle.jsx:71694311/3764

--- src/hooks/wiggle.jsx
+++ src/hooks/wiggle.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useCallback } from "react";
-import { useSpring } from "react-spring";
+import { useSpring } from "@react-spring/web";
 import { usePrefersReducedMotion } from "./prefers-reduced-motion";
-// Heavily inspired by Josh Comeau: https://www.joshwcomeau.com/react/boop/ 💖
 
+// Heavily inspired by Josh Comeau: https://www.joshwcomeau.com/react/boop/ 💖
 // Wiggle function accepts various parameters specifying properties for the animation
 export function useWiggle({
   x = 0,
@@ -17,10 +17,8 @@ export function useWiggle({
 }) {
   // Accessibility setting from the user system indicating that they prefer to minimize motion
   const prefersReducedMotion = usePrefersReducedMotion();
-
   // Declare state variable isActive, set initially to false
   const [isActive, setIsActive] = useState(false);
-
   // We offload the actual animation to spring: https://www.react-spring.io/docs/hooks/use-spring
   const style = useSpring({
     transform: isActive
@@ -28,7 +26,6 @@ export function useWiggle({
       : `translate(0px, 0px) rotate(0deg) scale(1)`,
     config: springConfig
   });
-
   // Timing parameter determines how long the wiggle lasts using browser setTimeout function
   // React useEffect function https://reactjs.org/docs/hooks-effect.html
   React.useEffect(() => {
@@ -42,14 +39,11 @@ export function useWiggle({
       window.clearTimeout(timeoutId);
     };
   }, [isActive]); // Continue wiggle until isActive is set false when timeout elapses
-
   // Set wiggle to active when the triggering event occurs - will be set false when effect completes above
   const trigger = useCallback(() => {
     setIsActive(true);
   }, []);
-
   let appliedStyle = prefersReducedMotion ? {} : style;
-
   // Return animation style effect and function to apply on trigger in page
   return [appliedStyle, trigger];
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { animated } from "react-spring";
+import { animated } from "@react-spring/web"
 import { useWiggle } from "../hooks/wiggle";
 import { Link } from "wouter";
 
